78b4472ee12c8870279f866fc85327c4f9797fb4
Alex Gerstein <gerstein.alex@gmail.com>
2013-10-22 04:19:05 -0400

    Add config to heroku

---
+++ config.py
@@ -0,0 +1,14 @@
+# -*- coding: utf8 -*-
+
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+CSRF_ENABLED = True
+SECRET_KEY = 'youll-never-guess'
+
+if os.environ.get('DATABASE_URL') is None:
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db') + '?check_same_thread=False'
+else:
+    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
+SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
+SQLALCHEMY_RECORD_QUERIES = True
