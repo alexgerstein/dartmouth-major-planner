771123ef26a56a6ea6cb67c316a23cbe332c7466
Alex Gerstein <gerstein.alex@gmail.com>
2013-12-03 11:07:20 -0500

    Added templates specification to flask initialiaztion

--- app/__init__.py
+++ app/__init__.py
@@ -7,7 +7,10 @@ from flask_cas import flask_cas
 # Alex Gerstein
 # Initialize the Flask app according to config, views, and models
 
-app = Flask(__name__)
+tmpl_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'templates')
+
+
+app = Flask(__name__, template_folder=tmpl_dir)
 app.config.from_object('config')
 
 app.register_blueprint(flask_cas)
