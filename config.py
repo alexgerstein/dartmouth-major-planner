45698df1c0d6b4f520798a1228eb32e1f8b9cf9d
Alex Gerstein <gerstein.alex@gmail.com>
2013-12-04 11:44:58 -0500

    Changed to unicode

--- config.py
+++ config.py
@@ -9,6 +9,6 @@ SECRET_KEY = 'youll-never-guess'
 if os.environ.get('DATABASE_URL') is None:
     SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db') + '?check_same_thread=False'
 else:
-    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
+    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL'] + '?charset=utf8'
 SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
 SQLALCHEMY_RECORD_QUERIES = True
