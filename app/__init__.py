5f4a0a0043e89012ee52a9176f256b3850ba3a75
Alex Gerstein <gerstein.alex@gmail.com>
2013-12-11 23:23:42 -0500

    Add logging

--- app/__init__.py
+++ app/__init__.py
@@ -12,13 +12,10 @@ app.config.from_object('config')
 
 if not app.debug:
     import logging
-    from logging.handlers import RotatingFileHandler
-    file_handler = RotatingFileHandler('tmp/dartplan.log', 'a', 1 * 1024 * 1024, 10)
-    file_handler.setFormatter(logging.Formatter('%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
+    stream_handler = logging.StreamHandler()
+    app.logger.addHandler(stream_handler)
     app.logger.setLevel(logging.INFO)
-    file_handler.setLevel(logging.INFO)
-    app.logger.addHandler(file_handler)
-    app.logger.info('DartPlan Startup')
+    app.logger.info('microblog startup')
 
 app.register_blueprint(flask_cas)
 
