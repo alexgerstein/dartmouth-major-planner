67b4bc616fa74ac72677162936a91f8726705202
Alex Gerstein <gerstein.alex@gmail.com>
2013-12-11 22:13:11 -0500

    Redirect to page not found correctly

--- app/views.py
+++ app/views.py
@@ -356,3 +356,8 @@ def edit():
 @app.errorhandler(404)
 def page_not_found(error):
     return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def internal_error(error):
+
+    return render_template('500.html'), 500
