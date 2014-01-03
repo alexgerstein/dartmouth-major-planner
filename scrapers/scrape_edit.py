55025dccaacc314ef7d286a82c59964f102168a1
Alex Gerstein <gerstein.alex@gmail.com>
2014-01-03 00:33:06 -0500

    filter term

--- scrapers/scrape_edit.py
+++ scrapers/scrape_edit.py
@@ -13,10 +13,16 @@ from scrape_curr_orc import *
 from scrape_old_orcs import *
 from scrape_timetable import *
 
-dept = Department.query.filter_by(abbr="COSC").first()
-course = Course.query.filter_by(dept = dept, number = "50").first()
+dept = Department.query.filter_by(abbr = "COSC").first()
+print dept
+
+course = Course.query.filter_by(department = dept, number = "50").first()
+print course
 
 term = Term.query.filter_by(season = "W", year = "2014").first()
+print term
+
 offering = Offering.query.filter_by(course = course, term = term).first()
+print offering
 
 print User.query.filter_by(User.courses.contains(offering)).count()
