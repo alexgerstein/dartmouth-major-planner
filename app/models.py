8bc7025f5bf91ab7b9c651fc5d77ddcbbabdf686
Alex Gerstein <gerstein.alex@gmail.com>
2014-04-16 18:18:14 -0400

    Extend hour string max length

--- app/models.py
+++ app/models.py
@@ -315,7 +315,7 @@ class Hour(db.Model):
 	__tablename__ = "hour"
 
 	id = db.Column(db.Integer, primary_key = True)
-	period = db.Column(db.String(50), index = True, unique = True)
+	period = db.Column(db.String(150), index = True, unique = True)
 	offerings = db.relationship('Offering', backref = 'hour')
 
 	def __init__(self, period):
