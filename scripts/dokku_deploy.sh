0bdb0d3c734d5fb3db3d4fd8d6ff4fa885e07c69
Alex Gerstein <gerstein.alex@gmail.com>
2015-04-27 17:16:22 -0400

    Specify permissions.

--- scripts/dokku_deploy.sh
+++ scripts/dokku_deploy.sh
@@ -1,5 +1,6 @@
 if ([ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_BRANCH" == "production" ]); then
   echo -e "Host dartplan.com\n\tStrictHostKeyChecking no\n" >> ~/.ssh/config
+  chmod 600 config/deploy_id_rsa
   eval `ssh-agent`
   ssh-add config/deploy_id_rsa
   git remote add dokku dokku@dartplan.com:dartplan
