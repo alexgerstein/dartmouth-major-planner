e8bbcd6706b842bb16e5136664130cd31852fcf1
Alex Gerstein <gerstein.alex@gmail.com>
2015-04-27 16:04:42 -0400

    Skip connection check.

--- scripts/dokku_deploy.sh
+++ scripts/dokku_deploy.sh
@@ -1,4 +1,5 @@
 if ([ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_BRANCH" == "production" ]); then
+  echo -e "Host dartplan.com\n\tStrictHostKeyChecking no\n" >> ~/.ssh/config
   git remote add dokku dokku@dartplan.com:dartplan
   git push dokku production
 fi
