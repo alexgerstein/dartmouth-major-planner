a5b32e5fd0aa828ffdb43c68b8056d073d0187cb
Alex Gerstein <gerstein.alex@gmail.com>
2016-06-04 15:36:27 -0700

    Add option to mark plan for 5-year program

--- dartplan/api/plans.py
+++ dartplan/api/plans.py
@@ -1,5 +1,4 @@
-from flask import g
-from flask.ext.restful import Resource, marshal, fields
+from flask.ext.restful import Resource, marshal, fields, reqparse, inputs
 
 from dartplan.authorization import plan_owned_by_user
 from dartplan.login import login_required
@@ -16,13 +15,39 @@ class getPlanTerms(fields.Raw):
 plan_fields = {
     'title': fields.String,
     'terms': getPlanTerms,
-    'offerings': fields.List(fields.Nested(offering_fields))
+    'offerings': fields.List(fields.Nested(offering_fields)),
+    'fifth_year': fields.Boolean
 }
 
 
 class PlanAPI(Resource):
+    def __init__(self):
+        self.reqparse = reqparse.RequestParser()
+        self.reqparse.add_argument('fifth_year', type=inputs.boolean)
+        super(PlanAPI, self).__init__()
+
     @plan_owned_by_user
     @login_required
     def get(self, id):
         plan = Plan.query.get_or_404(id)
+        return {'plan': marshal(plan, plan_fields)}
+
+    @plan_owned_by_user
+    @login_required
+    def put(self, id):
+        args = self.reqparse.parse_args()
+
+        plan = Plan.query.get_or_404(id)
+
+        if args.fifth_year is not None:
+            if args.fifth_year and plan.fifth_year:
+                return {"errors":
+                        {"fifth_year": ["Plan already has 5th year."]}}, 409
+            elif not args.fifth_year and not plan.fifth_year:
+                return {"errors":
+                        {"fifth_year":
+                            ["Plan already excludes 5th year."]}}, 409
+
+            plan.swap_fifth_year()
+
         return {'plan': marshal(plan, plan_fields)}
--- tests/test_api/test_plans.py
+++ tests/test_api/test_plans.py
@@ -17,3 +17,46 @@ class TestPlanAPI(TestBase):
         offering = plan_with_offering.offerings.first()
         assert plan_data['title'] == 'Default'
         assert plan_data['offerings'][0]['name'] == str(offering)
+
+    def test_enroll_in_fifth_year(self, test_client, plan_with_offering):
+        with test_client.session_transaction() as sess:
+            sess['user'] = {'netid': plan_with_offering.user.netid}
+
+        fifth_year = dict(fifth_year=True)
+        enroll_in_extra_year = test_client.put('/api/plans/%d' %
+                                               (plan_with_offering.id),
+                                               data=fifth_year)
+        self.check_valid_header_type(enroll_in_extra_year.headers)
+
+        data = json.loads(enroll_in_extra_year.data)
+
+        assert data['plan']['fifth_year'] is True
+
+    def test_enroll_in_fifth_year_already_enrolled(self, test_client,
+                                                   five_year_plan):
+        with test_client.session_transaction() as sess:
+            sess['user'] = {'netid': five_year_plan.user.netid}
+
+        fifth_year = dict(fifth_year=True)
+        enroll_in_extra_year = test_client.put('/api/plans/%d' %
+                                               (five_year_plan.id),
+                                               data=fifth_year)
+        self.check_valid_header_type(enroll_in_extra_year.headers)
+        assert enroll_in_extra_year.status_code == 409
+
+        data = json.loads(enroll_in_extra_year.data)
+        assert "Plan already has 5th year" in data['errors']['fifth_year'][0]
+
+    def test_unenroll_in_fifth_year_already_unenrolled(self, test_client,
+                                                       plan):
+        with test_client.session_transaction() as sess:
+            sess['user'] = {'netid': plan.user.netid}
+
+        fifth_year = dict(fifth_year=False)
+        enroll_in_extra_year = test_client.put('/api/plans/%d' %
+                                               (plan.id), data=fifth_year)
+        self.check_valid_header_type(enroll_in_extra_year.headers)
+        assert enroll_in_extra_year.status_code == 409
+
+        data = json.loads(enroll_in_extra_year.data)
+        assert "Plan already excludes 5th year" in data['errors']['fifth_year'][0]
